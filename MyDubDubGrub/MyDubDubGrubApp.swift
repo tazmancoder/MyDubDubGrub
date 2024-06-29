c030aa1bcc01e1466ed2b7f04f13635e298bc4c8
Mark J Perryman <tazmancoder@gmail.com>
2024-06-29 11:22:04 -0600

    Initial Commit

---
+++ MyDubDubGrub/MyDubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDubDubGrubApp.swift
+//  MyDubDubGrub
+//
+//  Created by Mark Perryman on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
