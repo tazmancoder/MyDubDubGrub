6ae7034c7d54b7cfad28e83d93d91529af0a6f3a
Mark J Perryman <tazmancoder@gmail.com>
2024-06-29 16:06:28 -0600

    Update 06-29-24 @16:06
    Finished video on MapView - Show the Map

--- MyDubDubGrub/LocationMapView.swift
+++ MyDubDubGrub/LocationMapView.swift
@@ -5,11 +5,32 @@
 //  Created by Mark Perryman on 6/29/24.
 //
 
+// San Jose Conventions Center Cooridnates
+// Latitue: 37.331516
+// Longitude: -121.891054
+
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+	// MARK: - State
+	@State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+	
     var body: some View {
-        Text("Map View")
+		ZStack {
+			Map(coordinateRegion: $region)
+				.ignoresSafeArea()
+			
+			VStack {
+				Image(.ddgMapLogo)
+					.resizable()
+					.scaledToFit()
+					.frame(height: 70)
+					.shadow(radius: 10)
+					
+				Spacer()
+			}
+		}
     }
 }
 
