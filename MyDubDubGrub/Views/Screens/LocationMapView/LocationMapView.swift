a4ee4d0bf6d123055f0c40c4bc96362589e68e1e
Mark J Perryman <tazmancoder@gmail.com>
2024-06-30 16:01:00 -0600

    Update 06-30-24 @16:00
    Finished video MVVM Setup

--- MyDubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
+++ MyDubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
@@ -14,12 +14,11 @@ import MapKit
 
 struct LocationMapView: View {
 	// MARK: - State
-	@State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-	@State private var alertItem: AlertItem?
+	@StateObject private var viewModel = LocationMapViewModel()
 	
     var body: some View {
 		ZStack {
-			Map(coordinateRegion: $region)
+			Map(coordinateRegion: $viewModel.region)
 				.ignoresSafeArea()
 			
 			VStack {
@@ -28,18 +27,11 @@ struct LocationMapView: View {
 				Spacer()
 			}
 		}
-		.alert(item: $alertItem, content: { alertItem in
+		.alert(item: $viewModel.alertItem, content: { alertItem in
 			Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
 		})
 		.onAppear {
-			CloudKitManager.getLocations { result in
-				switch result {
-				case .success(let locations):
-					print(locations)
-				case .failure(_):
-					alertItem = AlertContext.unableToGetLocations
-				}
-			}
+			viewModel.getLocations()
 		}
     }
 }
---
+++ MyDubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  LocationMapViewModel.swift
+//  MyDubDubGrub
+//
+//  Created by Mark Perryman on 6/30/24.
+//
+
+import MapKit
+
+final class LocationMapViewModel: ObservableObject {
+	// MARK: - Properties
+	@Published var alertItem: AlertItem?
+	@Published var region = MKCoordinateRegion(
+		center: CLLocationCoordinate2D(
+			latitude: 37.331516,
+			longitude: -121.891054
+		),
+		span: MKCoordinateSpan(
+			latitudeDelta: 0.01,
+			longitudeDelta: 0.01
+		)
+	)
+	@Published var locations: [DDGLocation] = []
+	
+	// MARK: - Functions
+	func getLocations() {
+		CloudKitManager.getLocations { [self] result in
+			switch result {
+			case .success(let locations):
+				self.locations = locations
+			case .failure(_):
+				alertItem = AlertContext.unableToGetLocations
+			}
+		}
+	}
+}
