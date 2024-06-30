5cc47f701d7d375f26f7199d939c86f2670b6c94
Mark J Perryman <tazmancoder@gmail.com>
2024-06-30 09:06:21 -0600

    Update 06-30-24 @09:06
    Finished video on Model - DDGLocation

---
+++ MyDubDubGrub/Model/DDGLocation.swift
@@ -0,0 +1,44 @@
+//
+//  DDGLocation.swift
+//  MyDubDubGrub
+//
+//  Created by Mark Perryman on 6/30/24.
+//
+
+import CloudKit
+
+struct DDGLocation {
+	// MARK: - Stringly typed constants
+	static let kName = "name"
+	static let kDescription = "description"
+	static let kSquareAsset = "squareAsset"
+	static let kBannerAsset = "bannerAsset"
+	static let kAddress = "address"
+	static let kLocation = "location"
+	static let kWebsiteURL = "websiteURL"
+	static let kPhoneNumber = "phoneNumber"
+	
+	// MARK: - Data Properties
+	let ckRecordID: CKRecord.ID
+	let name: String
+	let description: String
+	let squareAsset: CKAsset!
+	let bannerAsset: CKAsset!
+	let address: String
+	let location: CLLocation
+	let websiteURL: String
+	let phoneNumber: String
+	
+	init(record: CKRecord) {
+		ckRecordID = record.recordID
+		name = record[DDGLocation.kName] as? String ?? "N/A"
+		description = record[DDGLocation.kDescription] as? String ?? "N/A"
+		squareAsset = record[DDGLocation.kSquareAsset] as? CKAsset
+		bannerAsset = record[DDGLocation.kBannerAsset] as? CKAsset
+		address = record[DDGLocation.kAddress] as? String ?? "N/A"
+		location = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+		websiteURL = record[DDGLocation.kWebsiteURL] as? String ?? "N/A"
+		phoneNumber = record[DDGLocation.kPhoneNumber] as? String ?? "N/A"
+	}
+	
+}
