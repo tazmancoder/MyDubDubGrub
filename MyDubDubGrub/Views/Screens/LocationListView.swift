266a5c85c3f0245fe6713a03675819cc982f2fdb
Mark J Perryman <tazmancoder@gmail.com>
2024-07-02 19:04:25 -0600

    Update 07-02-24 @19:03
    Finished video Open Maps For Directions & Make A Call

--- MyDubDubGrub/Views/Screens/LocationListView.swift
+++ MyDubDubGrub/Views/Screens/LocationListView.swift
@@ -14,7 +14,11 @@ struct LocationListView: View {
     var body: some View {
 		List {
 			ForEach(locationManager.locations) { location in
-				NavigationLink(destination: LocationDetailView(location: location)) {
+				NavigationLink(
+					destination: LocationDetailView(
+						viewModel: LocationDetailViewModel(location: location)
+					)
+				) {
 					LocationCell(location: location)
 				}
 			}
